fc54aeecf94fc7c79bd9fd96a4b591fea0ffe08e
Hiro Watari <watari.codes@gmail.com>
2020-04-16 16:12:00 -0700

    Added User Story with expected failure

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,29 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
+
+# User visits an online to-do app
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# User checks that 'To-Do' is in the title
+assert 'To-Do' in browser.title
+
+# User is invited to enter a to-do item
+
+# User types "Buy peacock feathers" into a text box
+
+# When user hits <Enter>, the page updates and lists
+# "1: Buy peacock feathers"
+
+# The text box still allows the user to enter more items
+# User adds "Use peacock feathers to make a fly"
+
+# The page updates and lists both items
+
+# The site has generated a unique URL to access the to-do list
+
+# User visits that URL and confirms the list
+
+# User is done with the site
+
+browser.quit()

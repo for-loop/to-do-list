e042df3a70fabfd50ae88925a975607de4e951e1
Hiro Watari <watari.codes@gmail.com>
2020-04-16 16:24:58 -0700

    FT uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,29 +1,39 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
 
-# User visits an online to-do app
-browser.get('http://localhost:8000')
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# User checks that 'To-Do' is in the title
-assert 'To-Do' in browser.title
+    def tearDown(self):
+        self.browser.quit()
 
-# User is invited to enter a to-do item
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # User visits an online to-do app
+        self.browser.get('http://localhost:8000')
 
-# User types "Buy peacock feathers" into a text box
+        # User checks that 'To-Do' is in the title
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
-# When user hits <Enter>, the page updates and lists
-# "1: Buy peacock feathers"
+        # User is invited to enter a to-do item
 
-# The text box still allows the user to enter more items
-# User adds "Use peacock feathers to make a fly"
+        # User types "Buy peacock feathers" into a text box
 
-# The page updates and lists both items
+        # When user hits <Enter>, the page updates and lists
+        # "1: Buy peacock feathers"
 
-# The site has generated a unique URL to access the to-do list
+        # The text box still allows the user to enter more items
+        # User adds "Use peacock feathers to make a fly"
 
-# User visits that URL and confirms the list
+        # The page updates and lists both items
 
-# User is done with the site
+        # The site has generated a unique URL to access the to-do list
 
-browser.quit()
+        # User visits that URL and confirms the list
+
+        # User is done with the site
+
+if __name__ == '__main__':
+    unittest.main()
